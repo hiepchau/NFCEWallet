f86b07800909fae3d03ad5c0d56135ba27e987a8
hiepchau <85739811+hiepchau@users.noreply.github.com>
2023-06-19 11:54:26 +0700

    cacth exception

--- lib/ui/screen/authenticate/login/authenticate_bloc.dart
+++ lib/ui/screen/authenticate/login/authenticate_bloc.dart
@@ -32,7 +32,7 @@ class AuthenticationBloc extends Bloc<AuthenticationEvent, AuthenticationInfoSta
         if(e is DioError){
           print(e.response!.data);
         }
-        print("Log in failed");
+        print("Log in failed due to exception: $e");
 
         emit(AuthenticationInfoState(authenStatus: authenticateStatus.unAuthorized));
       }
